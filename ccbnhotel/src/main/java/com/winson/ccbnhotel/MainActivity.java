a33642df5de98d8fb99342b6aa27b9d16e0982a1
wenxiang <zhouwenxiang@km.com>
2018-07-15 18:56:38 +0800

    add image layout;

--- ccbnhotel/src/main/java/com/winson/ccbnhotel/MainActivity.java
+++ ccbnhotel/src/main/java/com/winson/ccbnhotel/MainActivity.java
@@ -7,15 +7,24 @@ import android.view.View;
 
 public class MainActivity extends BaseActivity {
 
+    View test;
+    boolean onFocus;
+    View content;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        test = findViewById(R.id.test);
+        content = findViewById(R.id.content);
+
         findViewById(R.id.enter).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                enter();
+//                enter();
+                content.setScaleX(1.15f);
+                content.setScaleY(1.15f);
             }
         });
 
