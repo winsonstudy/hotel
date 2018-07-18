5a35af5919476e438534af841e46ab831cbd18f4
wenxiang <929566854@qq.com>
2018-07-18 19:08:46 +0800

    update layout;

--- ccbnhotel/src/main/java/com/winson/ccbnhotel/MainActivity.java
+++ ccbnhotel/src/main/java/com/winson/ccbnhotel/MainActivity.java
@@ -7,8 +7,6 @@ import android.view.View;
 
 public class MainActivity extends BaseActivity {
 
-    View test;
-    boolean onFocus;
     View content;
 
     @Override
@@ -16,25 +14,8 @@ public class MainActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        test = findViewById(R.id.test);
         content = findViewById(R.id.content);
 
-        findViewById(R.id.enter).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-//                enter();
-                content.setScaleX(1.15f);
-                content.setScaleY(1.15f);
-            }
-        });
-
-
-
     }
 
-    private void enter(){
-        Intent intent = new Intent(this, HotelHomeActivity.class);
-        startActivity(intent);
-        finish();
-    }
 }
