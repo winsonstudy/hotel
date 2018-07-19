cdc037ad412432a844fdc6154d1273ac790d8a1e
wenxiang <929566854@qq.com>
2018-07-19 09:26:41 +0800

    update layout;

--- ccbnhotel/src/main/java/com/winson/ccbnhotel/MainActivity.java
+++ ccbnhotel/src/main/java/com/winson/ccbnhotel/MainActivity.java
@@ -1,21 +1,54 @@
 package com.winson.ccbnhotel;
 
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
+import android.os.Handler;
+import android.widget.TextView;
+
+import java.util.Calendar;
 
 public class MainActivity extends BaseActivity {
 
-    View content;
+    TextView timeTv, dateTv;
+    Handler handler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        content = findViewById(R.id.content);
+        handler = new Handler();
 
+        timeTv = findViewById(R.id.time);
+        dateTv = findViewById(R.id.date);
+
+        updateTime();
+        handler.postDelayed(timeRunnable, 500);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        handler.removeCallbacks(timeRunnable);
+    }
+
+    Runnable timeRunnable = new Runnable() {
+        @Override
+        public void run() {
+            updateTime();
+            handler.postDelayed(timeRunnable, 500);
+        }
+    };
+
+    public void updateTime() {
+        Calendar c = Calendar.getInstance();
+        int year = c.get(Calendar.YEAR);
+        int month = c.get(Calendar.MONTH) + 1;
+        int day = c.get(Calendar.DAY_OF_MONTH);
+        int hour = c.get(Calendar.HOUR_OF_DAY);
+        int minute = c.get(Calendar.MINUTE);
+
+        timeTv.setText(String.format("%02d:%02d", hour, minute));
+        dateTv.setText(String.format("%d/%02d/%02d", year, month, day));
     }
 
 }
